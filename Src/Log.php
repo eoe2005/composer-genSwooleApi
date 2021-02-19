6933226c434999c38837f583a06e9df4d265e4cb
pi <ghf@pi.com>
2021-02-19 10:45:43 +0000

    bug fix

--- Src/Log.php
+++ Src/Log.php
@@ -11,13 +11,19 @@ class Log
     }
 
     static function Error($format){
-        call_user_func_array('\\Gen\\Log::ServerDebug',func_get_args());
-
+        $data = func_get_args();
+        self::save('ERROR',...$data);
     }
-    static function Debug($format){}
-    static function Sql($format){}
+    static function Debug($format){
+        $data = func_get_args();
+        self::save('DEBUG',...$data);
+    }
+    static function Sql($format){
+        $data = func_get_args();
+        self::save('SQL',...$data);
+    }
     static function Wasd(){}
-    private static function save($data){
-
+    private static function save($type,$format){
+        echo sprintf("[%s] %s %s\n",date("Y-m-d H:i:s"),$type,call_user_func_array('sprintf',func_get_args()));
     }
 }
