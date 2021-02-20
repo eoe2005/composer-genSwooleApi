b0ac794e2fac8141255ec4bc47171803d59cbdd3
pi <ghf@pi.com>
2021-02-20 06:29:22 +0000

    增加获取参数的方法

--- Src/Action.php
+++ Src/Action.php
@@ -42,4 +42,57 @@ abstract class Action
         return $this->_new_uid;
     }
 
+    protected function getParam($key,$rouleString = "",$error = ""){
+        $ret = $this->params['data'][$key] ?? "";
+        if(!$rouleString){
+            return $ret;
+        }
+
+        $rules = explode(":",$rouleString);
+        foreach ($rules as $k){
+            $isError = false;
+            switch ($k){
+                case 'required':
+                    if(!$ret || !isset($this->params['data'][$key])){
+                        $isError = true;
+                    }
+                    break;
+                case "int":
+                    if(!preg_match("/\d+/",$ret)){
+                        $isError = true;
+                    }
+                    break;
+                case 'mobile':
+                    if(!preg_match("/1(35789)\d{9}/",$ret)){
+                        $isError = true;
+                    }
+                    break;
+                case 'email':
+                    if(!preg_match("/(.)+@.+\..{2}/",$ret)){
+                        $isError = true;
+                    }
+                    break;
+                case 'date':
+                    if(!preg_match("/\d{4}-\d{2}-\d{2}/",$ret)){
+                        $isError = true;
+                    }
+                    break;
+                case 'datetime':
+                    if(!preg_match("/\d{4}-\d{2}-\d{2} (0|1)\d:(0-5)\d:(0-5)\d/",$ret)){
+                        $isError = true;
+                    }
+                    break;
+                case 'time':
+                    if(!preg_match("/(0|1)\d:(0-5)\d:(0-5)\d/",$ret)){
+                        $isError = true;
+                    }
+                    break;
+            }
+            if($isError){
+                throw new \Exception("参数错误",303);
+            }
+        }
+        return $ret;
+    }
+
 }
